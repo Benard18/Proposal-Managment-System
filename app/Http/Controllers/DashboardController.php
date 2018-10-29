ec11bfbf6de6d9f292256d2f825b878ad58290ed
Ben Karanja <benkaranja43@gmail.com>
2018-10-29 18:27:34 +0300

    THis is my dashboard controller based on the user side

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use App\Admin;
+use App\Approve;
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+            $User_id = auth()->user()->id;
+        $user = User::find($User_id);
+
+          $id = Auth::user()->id;
+        $approve = Approve::all();
+        return view('dashboard')->with(array('proposals'=>$user->proposals,'approve'=>$approve));
+    }
+}
